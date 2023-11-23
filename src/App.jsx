a2e5c53b487856af67c4b071041a69e39cefb78e
brainstromsurfer <wishmeheaven@gmail.com>
2023-11-23 22:08:03 +0200

    page not found Route test3

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,26 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider, Route, Routes } from "react-router-dom";
 import { Warning, Welcome, Game, NotFound } from "./pages";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-
 import SharedLayout from "./layouts/SharedLayout";
 
-const routes = [
-  {
-    index: true,
-    element: <Warning />,
-  },
-  {
-    path: "intro",
-    element: <Welcome />,
-  },
-  {
-    path: "/",
-    element: <SharedLayout />,
-    children: [
-      {
-        path: "/quiz",
-        element: <Game />,
-      }      
-    ],
-  },
-  {
-    path: "*",
-    element: <NotFound />,
-  },
-];
+const App = () => {
+  const router = createBrowserRouter();
 
-function App() {
-  const router = createBrowserRouter(routes);
-  return <RouterProvider router={router} />;
-}
+  return (
+    <RouterProvider router={router}>
+      <Routes>
+        <Route
+          path="/"
+          element={<SharedLayout />}
+        >
+          <Route index element={<Warning />} />
+          <Route path="intro" element={<Welcome />} />
+          <Route path="quiz" element={<Game />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </RouterProvider>
+  );
+};
 
 export default App;
