580083eefb15585174fa8f70e9f5ba889570285d
brainstromsurfer <wishmeheaven@gmail.com>
2023-11-23 05:43:54 +0200

    fixing routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,24 @@
 import { Warning, Welcome, Game, NotFound } from "./pages";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import SharedLayout from "./layouts/SharedLayout";
 
 const routes = [
-  {
-    index: true,
-    element: <Warning />,
-  },
-  {
-    path: "intro",
-    element: <Welcome />,
-  },
   {
     path: "/",
     element: <SharedLayout />,
     children: [
       {
-        path: "/quiz",
+        index: true,
+        element: <Warning />,
+      },
+      {
+        path: "intro",
+        element: <Welcome />,
+      },
+      {
+        path: "quiz",
         element: <Game />,
-      },      
+      },
     ],
   },
   {
